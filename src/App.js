8c8b0c9aaef10906840bec30ea6f7268623bdb9c
ALVIN VARGHESE <alvinvargheseavm@gmail.com>
2023-05-17 10:12:22 +0530

    2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import './App.css'
 import Hero from './Components/Heropage/Hero'
-
 function App() {
   return (
     <div>
--- src/Components/Heropage/Hero.js
+++ src/Components/Heropage/Hero.js
@@ -4,13 +4,26 @@ import store from './Buy at online shop - 2004x1500 1.png'
 function Hero() {
   return (
     <div className='hero'>
+      <section className='section1'>
         <div className='hero__content'>
                 <p className='heading'>A Complete Rental Website for you</p>
                 <img className='store' src={store} alt="" />
                 <p className='description'>RentArc enables one to rent different products near them . We also provide functionality to give your products for rent to others </p>
                 <button className='rent'>Rent Now</button>
                 <button className='give'>Give for rent</button>
-        </div>    
+        </div> 
+      </section> 
+      <section className='section2'>
+      <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search this blog"></input>
+          <div class="input-group-append">
+             <button class="btn btn-secondary" type="button">
+               <i class="fa fa-search"></i>
+              </button>
+          </div>
+  </div>  
+        
+        </section>  
       
     </div>
   )
